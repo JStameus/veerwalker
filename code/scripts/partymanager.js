e7a322da1a6cef688fbedf8ab1083f277dc07fa4
JStameus <joseph.stameus@gmail.com>
2020-09-25 19:23:43 +0200

    added stuff

---
+++ code/scripts/partymanager.js
@@ -0,0 +1,8 @@
+let playerParty = [partyCharacter01, partyCharacter02];
+
+function Character(name, hitPoints, defense, attack) {
+    this.name = name;
+    this.hitPoints = hitPoints;
+    this.defense = defense;
+    this.attack = attack;
+}
