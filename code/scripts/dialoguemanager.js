c0feedbe57d6574bb6bcd766aa528b4f9f9e3915
JStameus <joseph.stameus@gmail.com>
2020-09-28 13:29:42 +0200

    added buttons to the dialogue window and assigned them in the dialogue choices

--- code/scripts/dialoguemanager.js
+++ code/scripts/dialoguemanager.js
@@ -8,12 +8,22 @@
 //     document.getElementById("dialogue_choice_text_04").innerHTML = dialogueChoice04;
 // }
 
+let playerName = "Big Jobo";
+
 let dialogueChoice01 = {
     text: "I'm not sure. Everything is a bit foggy.",
 }
 
 let dialogueChoice02 = {
-    text: ""
+    text: "Me? I am Lord Nerevar, of course! Don't you recognize me?",
+}
+
+let dialogueChoice03 = {
+    text: "I'm not telling you my name. I'm not sure I can trust you.",
+}
+
+let dialogueChoice04 = {
+    text: "My name is " + playerName + ". And you are...?",
 }
 
 let actorJiub = {
@@ -25,7 +35,7 @@ let dialogueNode01 = {
     location: "Prison Boat",
     actor: actorJiub,
     text: "Stand up. There you go. You were dreaming. What's your name?",
-    choices: [dialogueChoice01], 
+    choices: [dialogueChoice01, dialogueChoice02, dialogueChoice03, dialogueChoice04], 
 }
 
 let dialogueNode02 = {
@@ -38,4 +48,9 @@ let dialogueNode02 = {
 function refreshDialogueNode(dialogueNode) {
     document.getElementById("dialogue_text_main").innerHTML = dialogueNode.text;
     document.getElementById("dialogue_eventlabel").innerHTML = dialogueNode.location;
+    document.getElementById("choice_button_01").innerHTML = dialogueNode.choices[0].text;
+    document.getElementById("choice_button_02").innerHTML = dialogueNode.choices[1].text;
+    document.getElementById("choice_button_03").innerHTML = dialogueNode.choices[2].text;
+    document.getElementById("choice_button_04").innerHTML = dialogueNode.choices[3].text;
+
 }
