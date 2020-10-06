02ef7c8e7fd3a1d5563677395b8c119d63586cd1
JStameus <joseph.stameus@gmail.com>
2020-10-06 19:35:49 +0200

    made it possible to load dialogue tree files from disk and load html content from that file

--- code/scripts/dialoguemanager.js
+++ code/scripts/dialoguemanager.js
@@ -1,3 +1,35 @@
+//Which dialogue tree and node is currently active
+var dialogueTree = null;
+var currentDialogueNode =  null;
+
+//functions for loading new trees and nodes
+function loadDialogueTree(fileURL) {
+    fetch(fileURL).then(response => response.json()).then(json => {dialogueTree = json});
+}
+
+function loadDialogueNode(dialogueNode) {
+    if(dialogueTree != null)
+    {
+        locationText.innerHTML = dialogueNode.location;
+        dialogueText.innerHTML = dialogueNode.paragraphs[0].text;
+        choiceButton01.innerHTML = dialogueNode.responses[0].text;
+        choiceButton02.innerHTML = dialogueNode.responses[1].text;
+        choiceButton03.innerHTML = dialogueNode.responses[2].text;
+        choiceButton04.innerHTML = dialogueNode.responses[3].text;
+        currentDialogueNode = dialogueNode;
+    }
+    else 
+    {
+        console.warn("No Dialogue Tree has been loaded!");
+    }
+    //load a specific node from the current tree 
+    //add its location to the locationText
+    //loop through its paragraphs and add them to the dialogueText
+    //loop(?) through the responses and add them to the buttons
+    //set the current node to the node you just loaded
+}
+
+//All of the HTML elements that the node uses
 var locationText = document.getElementById("dialogue_locationlabel");
 var dialogueText = document.getElementById("dialogue_text_main");
 
@@ -25,19 +57,4 @@ choiceButton04.addEventListener("click", function() {
     //loadDialogueNode(currentDialogueNode.responses[3].nextNode);
 });
 
-var currentDialogueNode =  null;
-
-function loadDialogueTree() {
-    //load the json file for the dialogue tree
-}
-
-function loadDialogueNode() {
-    //load a specific node from the current tree 
-    //add its location to the locationText
-    //loop through its paragraphs and add them to the dialogueText
-    //loop(?) through the responses and add them to the buttons
-    //set the current node to the node you just loaded
-}
-
-
-
+document.onload = loadDialogueTree('/code/json/dialogue/dialogue_boat_wakeup.json');
