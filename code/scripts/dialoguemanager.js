ba1af250429a5df579537fced8c3fb06f13fec2f
JStameus <joseph.stameus@gmail.com>
2020-10-07 14:33:25 +0200

    added function for clearing and adding new dialogue buttons elements based on the current node.

--- code/scripts/dialoguemanager.js
+++ code/scripts/dialoguemanager.js
@@ -2,10 +2,10 @@
 var dialogueTree = null;
 var currentDialogueNode =  null;
 //Variables for storing the index of each responses next node
-var response01NextNode = null;
-var response02NextNode = null;
-var response03NextNode = null;
-var response04NextNode = null;
+// var response01NextNode = null;
+// var response02NextNode = null;
+// var response03NextNode = null;
+// var response04NextNode = null;
 
 //functions for loading new trees and nodes
 function loadDialogueTree(fileURL) {
@@ -15,33 +15,30 @@ function loadDialogueTree(fileURL) {
 function loadDialogueNode(nodeIndex) {
     if(dialogueTree != null)
     {
+        currentDialogueNode = dialogueTree.nodes[nodeIndex];
         locationText.innerHTML = dialogueTree.nodes[nodeIndex].location;
         dialogueText.innerHTML = dialogueTree.nodes[nodeIndex].paragraphs[0].text;
         choiceButton01.innerHTML = dialogueTree.nodes[nodeIndex].responses[0].text;
         choiceButton02.innerHTML = dialogueTree.nodes[nodeIndex].responses[1].text;
         choiceButton03.innerHTML = dialogueTree.nodes[nodeIndex].responses[2].text;
         choiceButton04.innerHTML = dialogueTree.nodes[nodeIndex].responses[3].text;
-        currentDialogueNode = dialogueTree.nodes[nodeIndex];
         response01NextNode = dialogueTree.nodes[nodeIndex].responses[0].nextNode;
         response02NextNode = dialogueTree.nodes[nodeIndex].responses[1].nextNode;
         response03NextNode = dialogueTree.nodes[nodeIndex].responses[2].nextNode;
         response04NextNode = dialogueTree.nodes[nodeIndex].responses[3].nextNode;
-        
     }
     else 
     {
         console.warn("No Dialogue Tree has been loaded!");
     }
-    //load a specific node from the current tree 
-    //add its location to the locationText
-    //loop through its paragraphs and add them to the dialogueText
-    //loop(?) through the responses and add them to the buttons
-    //set the current node to the node you just loaded
 }
 
 //All of the HTML elements that the node uses
 var locationText = document.getElementById("dialogue_locationlabel");
 var dialogueText = document.getElementById("dialogue_text_main");
+
+var choiceButtons = [];
+var responseNextNodes = [];
 
 var choiceButton01 = document.querySelector("#choice_button_01");
 choiceButton01.addEventListener("click",function(){
@@ -64,9 +61,12 @@ choiceButton04.addEventListener("click", function() {
     console.log("Clicked Button 4");
 });
 
-function updateChoiceButtons() {
-    
+function clearChoiceButtons() {
+    document.querySelectorAll('.dialogue_choice_container').forEach(function(a){a.remove()});
+}
 
+function updateChoiceButtons() {
+    clearChoiceButtons();
     //Creates new buttons based on the responses of the dialogue node
     for (var i = 0; i < currentDialogueNode.responses.length; i++)
     {
@@ -81,14 +81,7 @@ function updateChoiceButtons() {
         responsesWindow.appendChild(newDiv);
         console.log("Created a new Response Button!");
     }
-    //for each response in currentDialogueNode
-        //create a new div with the class 'dialogue_choice_container'
-        //create a new button with the class 'dialogue_choice_button'
-        //assign the innerText on the new button to the text of the response
-        //assign the nextNode of the corresponding choice button 
-        //append the whole thingamajig to the div 
-    //for each new div created
-        //append the divs to .dialogue_choices
+    //make the buttons clickable!
 }
 
 document.onload = loadDialogueTree('/code/json/dialogue/dialogue_boat_wakeup.json');
