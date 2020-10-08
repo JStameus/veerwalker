371c7450de39adca783ce5c8dd9a07ba389d0618
JStameus <joseph.stameus@gmail.com>
2020-10-08 14:00:15 +0200

    added functions for adding paragraphs when loading a node, as well as clearing paragraps when needed. Added clear button for testing purposes.

--- code/scripts/dialoguemanager.js
+++ code/scripts/dialoguemanager.js
@@ -17,7 +17,7 @@ function loadDialogueNode(nodeIndex) {
     {
         currentDialogueNode = dialogueTree.nodes[nodeIndex];
         locationText.innerHTML = dialogueTree.nodes[nodeIndex].location;
-        dialogueText.innerHTML = dialogueTree.nodes[nodeIndex].paragraphs[0].text;
+        addDialogueParagraphs();
         updateChoiceButtons();
     }
     else 
@@ -27,19 +27,41 @@ function loadDialogueNode(nodeIndex) {
 }
 
 function addDialogueParagraphs() {
-    //after dialogue node is loaded
-    //for each paragraph in node
-        //create a div 
-        //create a h3 
-        //create a paragraph
-        //set h3 to speaker name
-        //set paragraph to text
-        //append all to div
-        //append div to dialogue window
+    for(let i = 0; i < currentDialogueNode.paragraphs.length; i++)
+    {
+        let dialogueWindow = document.getElementById('dialogue_window');
+        //create elements
+        let newDiv = document.createElement('div');
+        let newLabel = document.createElement('h3');
+        let newLabelText = document.createTextNode(currentDialogueNode.paragraphs[i].speaker);
+        let newParagraph = document.createElement('p');
+        let newParagraphText = document.createTextNode(currentDialogueNode.paragraphs[i].text);
+        //assign classes and id
+        newDiv.className = "dialogue_paragraph";
+        newDiv.id = "paragraph_" + (i) + "_node_" + currentDialogueNode.nodeID;
+        console.log("Created " + newDiv.id);
+        newParagraph.className = "dialogue_paragraph_text";
+        //This solution is not optimal, look over it again
+        if(currentDialogueNode.paragraphs[i].narration == false)
+        {
+            newLabel.appendChild(newLabelText);
+            newDiv.appendChild(newLabel);
+        }
+        else 
+        {
+            newLabelText.remove;
+            newLabel.remove;
+        }
+        //append children
+        newParagraph.appendChild(newParagraphText);
+        newDiv.appendChild(newParagraph);
+        dialogueWindow.appendChild(newDiv);
+    }
 }
 
 function clearDialogueParagraphs () {
-    
+    let dialogueWindow = document.getElementById('dialogue_window');
+    dialogueWindow.innerHTML = '';
 }
 
 function clearChoiceButtons() {
@@ -50,21 +72,21 @@ function updateChoiceButtons() {
     if(currentDialogueNode != null)
     {
         clearChoiceButtons();
-        for (var i = 0; i < currentDialogueNode.responses.length; i++)
+        for (let i = 0; i < currentDialogueNode.responses.length; i++)
         {
-            const newDiv = document.createElement('div');
-            const newButton = document.createElement('button');
-            const newButtonText = document.createTextNode(currentDialogueNode.responses[i].text);
+            let newDiv = document.createElement('div');
+            let newButton = document.createElement('button');
+            let newButtonText = document.createTextNode(currentDialogueNode.responses[i].text);
             newDiv.className = "dialogue_choice_container";
             newButton.className = "dialogue_choice_button";
             newButton.id = "choice_button_" + (i + 1);
-            const nextNodeIndex = currentDialogueNode.responses[i].nextNode;
+            let nextNodeIndex = currentDialogueNode.responses[i].nextNode;
             newButton.addEventListener("click", function() {
                 loadDialogueNode(nextNodeIndex);
             });
             newButton.appendChild(newButtonText);
             newDiv.appendChild(newButton);
-            const responsesWindow = document.getElementById("dialogue_choices");
+            let responsesWindow = document.getElementById("dialogue_choices");
             responsesWindow.appendChild(newDiv);
         }
     }
@@ -80,3 +102,8 @@ var resetButton = document.getElementById("reset_button");
 resetButton.addEventListener("click", function() {
     loadDialogueNode(0);
 });
+
+var clearButton = document.getElementById('clear_button');
+clearButton.addEventListener('click', function() {
+    clearDialogueParagraphs();
+})
