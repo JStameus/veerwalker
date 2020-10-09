7f51ff2c1ee4c796b3ab28c61a93c4360cbf7ac9
JStameus <joseph.stameus@gmail.com>
2020-10-09 12:24:10 +0200

    added more dialogue to the test scene, and made functions in the script for debugging, checking which responses are missing a nextNode etc

--- code/scripts/dialoguemanager.js
+++ code/scripts/dialoguemanager.js
@@ -1,5 +1,6 @@
 //this is temporary, should be stored somewhere else later
 var playerName = 'Player';
+var checkPointNode = 0;
 
 //Which dialogue tree and node is currently active
 var dialogueTree = null;
@@ -141,13 +142,39 @@ function updateChoiceButtons() {
     }
 }
 
-document.onload = loadDialogueTree('/code/json/dialogue/dialogue_boat_wakeup.json');
+document.onload = loadDialogueTree('/code/json/dialogue/dialogue_test_escapepod.json');
+
+function checkNullNextNodes() {
+    let a = 0;
+    let b = 0;
+    for(a = 0; a < dialogueTree.nodes.length; a++)
+    {
+        console.log("--Node: " + dialogueTree.nodes[a].nodeIndex + " " + "ID: " + dialogueTree.nodes[a].nodeID + "--");
+        for(b = 0; b < dialogueTree.nodes[a].responses.length; b++)
+        {
+            if(dialogueTree.nodes[a].responses[b].nextNode == null)
+            {
+                console.warn("Response: " + b + " has a NULL nextNode!");
+            }
+        }
+    }
+}
+
+function checkParagraphCount(nodeIndex) {
+    console.log("--Node: " + dialogueTree.nodes[nodeIndex] + " ID: " + dialogueTree.nodes[nodeIndex].nodeID + "--");
+    console.log("Has " + dialogueTree.nodes[nodeIndex].responses.length + " paragraphs.");
+}
+
+function checkResponseCount(nodeIndex) {
+    console.log("--Node: " + dialogueTree.nodes[nodeIndex] + " ID: " + dialogueTree.nodes[nodeIndex].nodeID + "--");
+    console.log("Has " + dialogueTree.nodes[nodeIndex].paragraphs.length + " responses.");
+}
 
 //buttons for testing purposes, these are only temporary
 var resetButton = document.getElementById("reset_button");
 resetButton.addEventListener("click", function() {
     clearAllDialogueElements();
-    loadDialogueNode(0);
+    loadDialogueNode(checkPointNode);
 });
 
 var clearButton = document.getElementById('clear_button');
