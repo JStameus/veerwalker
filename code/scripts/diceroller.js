2d7b4b1d7fa8ca8426d761fcafa08708475d6a21
JStameus <joseph.stameus@gmail.com>
2020-10-18 12:39:12 +0200

    created encountermanager and diceroller scripts, and a page with styling to test it out

---
+++ code/scripts/diceroller.js
@@ -0,0 +1,11 @@
+function rollD4() {
+    return Math.floor(Math.random() * (4 - 1 + 1) ) + 1;
+}
+
+function rollD6() {
+    return Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
+}
+
+function rollD20() {
+    return Math.floor(Math.random() * (20 - 1 + 1) ) + 1;
+}
---
+++ code/scripts/encountermanager.js
@@ -0,0 +1,57 @@
+function Character(name, HP, AP, ATK, DEF, DMG) {
+    this.characterName = name;
+    this.maxHP = HP;
+    this.maxAP = AP;
+    this.attack = ATK;
+    this.defense = DEF;
+    this.damage = DMG;
+}
+
+let playerCharacter = new Character("Player", 25, 10, 3, 14, 2);
+let enemyCharacter = new Character("Enemy", 20, 10, 2, 12, 1);
+
+function loadEncounter() {
+
+}
+
+function createCharacterAvatar(character, position) {
+    let newName = document.createElement('h3');
+    let newNameText = document.createTextNode(character.characterName);
+    newName.appendChild(newNameText);
+    let newLine = document.createElement('hr');
+
+    let newHealthDisplay = document.createElement('p');
+    let newHealthDisplayText = document.createTextNode("HP: " + character.maxHP);
+    newHealthDisplay.appendChild(newHealthDisplayText);
+
+    let newActionPointDisplay = document.createElement('p');
+    let newActionPointDisplayText = document.createTextNode("AP: " + character.maxAP);
+    newActionPointDisplay.appendChild(newActionPointDisplayText);
+
+    let newAttackDisplay = document.createElement('p');
+    let newAttackDisplayText = document.createTextNode("ATK: " + character.attack);
+    newAttackDisplay.appendChild(newAttackDisplayText);
+
+    let newDefenseDisplay = document.createElement('p');
+    let newDefenseDisplayText = document.createTextNode("DEF: " + character.defense);
+    newDefenseDisplay.appendChild(newDefenseDisplayText);
+
+    let newDamageDisplay = document.createElement('p');
+    let newDamageDisplayText = document.createTextNode("DMG: " + character.damage);
+    newDamageDisplay.appendChild(newDamageDisplayText);
+
+    let newDiv = document.createElement('div');
+    newDiv.appendChild(newName);
+    newDiv.appendChild(newLine);
+    newDiv.appendChild(newHealthDisplay);
+    newDiv.appendChild(newActionPointDisplay);
+    newDiv.appendChild(newAttackDisplay);
+    newDiv.appendChild(newDefenseDisplay);
+    newDiv.appendChild(newDamageDisplay);
+
+    let arenaField = position;
+    arenaField.appendChild(newDiv);
+}
+
+let leftSide = document.getElementById("arena_leftside");
+let rightSide = document.getElementById("arena_rightside");
